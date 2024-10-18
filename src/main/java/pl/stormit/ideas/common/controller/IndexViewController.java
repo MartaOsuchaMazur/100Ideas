b1fb617e11e02c629724da7d93c01970591aa1b3
MartaOsuchaMazur <jagooo1@wp.pl>
2024-10-18 22:49:10 +0200

    added error's logger

--- src/main/java/pl/stormit/ideas/common/controller/IndexViewController.java
+++ src/main/java/pl/stormit/ideas/common/controller/IndexViewController.java
@@ -1,6 +1,9 @@
 package pl.stormit.ideas.common.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,15 +15,26 @@ import pl.stormit.ideas.question.service.QuestionService;
 import java.util.List;
 import java.util.UUID;
 
+
 @Controller
 @RequestMapping("/")
 @RequiredArgsConstructor
+@Slf4j
 public class IndexViewController extends IdeasCommonViewController {
+
+    Logger logger = LoggerFactory.getLogger(IndexViewController.class);
 
     private final QuestionService questionService;
 
     @GetMapping
     public String indexView(Model model) {
+
+        Exception exception = new RuntimeException("Error...");
+
+        logger.error("LoggerFactory example", exception);
+        logger.info("Hello EAI students!");
+        log.error("Lombok example", exception);
+
         addGlobalAttributes(model);
 
         List<QuestionDto> questionsTop = questionService.findTop(2);
